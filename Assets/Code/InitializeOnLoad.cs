ffc78a4863156ac27e0a0775ba1caf32a3bd47e9
liufeng <zgjllf@msn.cn>
2018-07-02 17:02:25 +0800

    no message

---
+++ Assets/Code/InitializeOnLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Hosting;
+using UnityEngine.SceneManagement;
+
+public class InitializeOnLoad : MonoBehaviour {
+
+	[RuntimeInitializeOnLoadMethod]
+	static void Initialize()
+	{
+        if (SceneManager.GetActiveScene().name == "Game" || 
+            SceneManager.GetActiveScene().name == "Logo" || 
+            SceneManager.GetActiveScene().name == "Menu" 
+           )
+		{
+            SceneManager.LoadScene("Gate");
+		}
+
+	}
+}
